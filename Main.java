243e2f7885b934398021ad272c111954ece68844
lmcbride5 <lilibeth.mcbride@patelhs.org>
2021-02-22 14:13:37 +0000

    Created the method and fixed some parts of the code

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner; 
+
 class Main {
   public static void main(String[] args) {
-  //code using recursion to call and add the amount of folders on a computer 
+  //code using recursion to call from the user and add figure the price and what color of the folders they want.  
 
+      Scanner scan =  new Scanner(System.in);
+
+      System.out.println("How many folders do you want to buy?"); 
+        int amountOfFolders = scan.nextInt(); 
+      System.out.println("What is the price of the folders?"); 
+        double foldersPrice = scan.nextDouble(); 
       
-    }
+      int i = 0; 
+
+      int result = folderCost(amountOfFolders, foldersPrice, i);
   }
+
+      static double folderCost(int amountOfFolders, double foldersPrice, int i)
+      {
+        if(i < amountOfFolders) 
+          i++; 
+        { 
+          return foldersPrice + folderCost(amountOfFolders, foldersPrice, i); 
+        }else{
+          return 0;
+        }
+      }
+    }
